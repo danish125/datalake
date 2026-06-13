1f9dc15e83e56bc1282cc567af0b5557417ea952
danish125 <drehman073@gmail.com>
2026-06-13 17:07:10 +0100

    init

--- main.tf
+++ main.tf
@@ -11,19 +11,50 @@ variable "vpc_id"   { default = "" } # use default VPC if empty
 
 data "aws_vpc" "default" { default = true }
 data "aws_subnets" "default" {
-  filter { name = "vpc-id"; values = [data.aws_vpc.default.id] }
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
 }
 
 resource "aws_security_group" "main_sg" {
   name   = "mysql-airbyte-minio-sg"
   vpc_id = data.aws_vpc.default.id
 
-  ingress { from_port = 22,    to_port = 22,    protocol = "tcp", cidr_blocks = ["0.0.0.0/0"] }
-  ingress { from_port = 3306,  to_port = 3306,  protocol = "tcp", cidr_blocks = ["10.0.0.0/8"] }
-  ingress { from_port = 8000,  to_port = 8000,  protocol = "tcp", cidr_blocks = ["0.0.0.0/0"] } # Airbyte UI
-  ingress { from_port = 9000,  to_port = 9001,  protocol = "tcp", cidr_blocks = ["0.0.0.0/0"] } # MinIO
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-  egress { from_port = 0, to_port = 0, protocol = "-1", cidr_blocks = ["0.0.0.0/0"] }
+  ingress {
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "tcp"
+    cidr_blocks = ["10.0.0.0/8"]
+  }
+
+  ingress {
+    from_port   = 8000
+    to_port     = 8000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 9000
+    to_port     = 9001
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
 # ---------------- MySQL EC2 ----------------
